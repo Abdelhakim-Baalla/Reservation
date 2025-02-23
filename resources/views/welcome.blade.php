e7cb062fb4776e74fcffdde3f48a1cf057af8165
Abdelhakim Baalla <abdelhakimbaalla50@gmail.com>
2025-02-23 20:50:02 +0000

    Change

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -12,8 +12,8 @@ Acceuil
             <div class="mb-4">
                 <i class="fas fa-laptop text-3xl"></i>
             </div>
-            <h1 class="text-4xl md:text-5xl font-bold mb-4">Nous connectons les gens pour donner vie à des projets</h1>
-            <p class="text-lg mb-6">Trouvez des talents de haute qualité ou des emplois ouverts à l'aide d'outils d'IA qui vous permettent de garder le contrôle.</p>
+            <h1 class="text-4xl md:text-5xl font-bold mb-4">Nous Aidon vous de Trouver Ton Salle</h1>
+            <p class="text-lg mb-6"></p>
             <div class="flex space-x-4">
                 <a href="#" class="bg-teal-500 text-white px-6 py-3 rounded-md shadow hover:bg-teal-600">En Savoir Plus</a>
                 <a href="#" class="bg-white text-gray-800 px-6 py-3 rounded-md shadow hover:bg-gray-200">Rejoignez-Nous</a>
